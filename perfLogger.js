448713d499aecfb36e0a9bfb107f3199acc0de92
Arnaud Benhamdine <arnaud.benhamdine@gmail.com>
2017-05-26 12:42:11 +0200

    Use singleton reference instead of this to avoid bad reference with this

--- perfLogger.js
+++ perfLogger.js
@@ -6,7 +6,7 @@ const moment = require('moment');
 const _ = require('lodash');
 const os = require('os');
 
-const myPerfLogger = {
+const self = {
 	_tests: {}, // to save tests
 	_testsSummary: [], //  to save totals nb and duration
 	_isEnabled: false,
@@ -25,30 +25,30 @@ const myPerfLogger = {
 
 		chai.expect(b).to.exist.and.be.a('boolean');
 
-		this._isEnabled = b;
+		self._isEnabled = b;
 	},
 
 	setConfig: function(config) {
 		chai.expect(config).to.exist.and.be.a('object').and.not.be.empty;
 
 		if (!_.isUndefined(config._printStartAndEnd) && !_.isNull(config._printStartAndEnd)) {
-			this.setPrintStartAndEnd(config._printStartAndEnd);
+			self.setPrintStartAndEnd(config._printStartAndEnd);
 		}
 
 		if (config._fSummaryHookFunction) {
-			this.setHookFunction(config._fSummaryHookFunction);
+			self.setHookFunction(config._fSummaryHookFunction);
 		}
 
 		if (!_.isUndefined(config._isEnabled) && !_.isNull(config._isEnabled)) {
-			this.enable(config._isEnabled);
+			self.enable(config._isEnabled);
 		}
 
 		if (!_.isUndefined(config._bDisplaySummaryWhenHook) && !_.isNull(config._bDisplaySummaryWhenHook)) {
-			this.setDisplaySummaryWhenHook(config._bDisplaySummaryWhenHook);
+			self.setDisplaySummaryWhenHook(config._bDisplaySummaryWhenHook);
 		}
 
 		if (config._iDefaultLevelMs) {
-			this.setDefaultCriticityTime(config._iDefaultLevelMs);
+			self.setDefaultCriticityTime(config._iDefaultLevelMs);
 		}
 
 	},
@@ -59,20 +59,20 @@ const myPerfLogger = {
 		}
 
 		chai.expect(b).to.exist.and.be.a('boolean');
-		this._printStartAndEnd = b;
+		self._printStartAndEnd = b;
 	},
 
 	setDefaultCriticityTime: function(iMs) {
 
 		chai.expect(iMs).to.be.a('number');
-		this._iDefaultCriticityTime = iMs;
+		self._iDefaultCriticityTime = iMs;
 
 	},
 
 	setDisplaySummaryWhenHook: function(b) {
 
 		chai.expect(b).to.be.a('boolean');
-		this._bDisplaySummaryWhenHook = b;
+		self._bDisplaySummaryWhenHook = b;
 
 	},
 
@@ -80,7 +80,7 @@ const myPerfLogger = {
 
 		chai.expect(sTag, "sTag devrait être une chaine non vide").to.exist.and.be.a('string').and.not.be.empty;
 
-		if (!this._isEnabled) {
+		if (!self._isEnabled) {
 			return;
 		}
 
@@ -90,34 +90,34 @@ const myPerfLogger = {
 
 		newTest.sName = sTag;
 
-		if (_.isUndefined(this._tests[sTag])) {
-			this._tests[sTag] = newTest;
+		if (_.isUndefined(self._tests[sTag])) {
+			self._tests[sTag] = newTest;
 		}
 
-		const indexSum = _.findIndex(this._testsSummary, {
+		const indexSum = _.findIndex(self._testsSummary, {
 			sName: sTag
 		});
 
 		if (-1 === indexSum) {
 
-			const indexLevel = _.findIndex(this._levels, {
+			const indexLevel = _.findIndex(self._levels, {
 				sName: sTag
 			});
 
 			let iLevel = 0;
 
 			if (-1 === indexLevel) {
-				const indexLevelAll = _.findIndex(this._levels, {
-					sName: this._LEVEL_TAG_ALL
+				const indexLevelAll = _.findIndex(self._levels, {
+					sName: self._LEVEL_TAG_ALL
 				});
 
 				if (-1 === indexLevelAll) {
 					iLevel = 10; // 10 ms default if no level defined
 				} else {
-					iLevel = this._levels[indexLevelAll].iLevel;
+					iLevel = self._levels[indexLevelAll].iLevel;
 				}
 			} else {
-				iLevel = this._levels[indexLevel].iDuration;
+				iLevel = self._levels[indexLevel].iDuration;
 			}
 
 			const newTestSummary = {
@@ -130,13 +130,13 @@ const myPerfLogger = {
 				iNbAboveLevel: 0,
 				ended : false
 			};
-			this._testsSummary.push(newTestSummary);
+			self._testsSummary.push(newTestSummary);
 
 		} else {
-			this._testsSummary[indexSum].iNb++;
+			self._testsSummary[indexSum].iNb++;
 		}
 
-		if (this._printStartAndEnd) {
+		if (self._printStartAndEnd) {
 
 			const sText = "Start %s";
 
@@ -147,12 +147,12 @@ const myPerfLogger = {
 
 	abort: function(sTag) {
 
-		if (!this._isEnabled) {
+		if (!self._isEnabled) {
 			return;
 		}
 
-		if (!_.isUndefined(this._tests[sTag])) {
-			delete this._tests[sTag];
+		if (!_.isUndefined(self._tests[sTag])) {
+			delete self._tests[sTag];
 		}
 
 	},
@@ -161,55 +161,55 @@ const myPerfLogger = {
 
 		chai.expect(sTag, "sTag devrait être une chaine non vide").to.exist.and.be.a('string').and.not.be.empty;
 
-		if (!this._isEnabled) {
+		if (!self._isEnabled) {
 			return;
 		}
 
-		if (_.isUndefined(this._tests[sTag])) {
+		if (_.isUndefined(self._tests[sTag])) {
 			console.error("Fail to end perf measurement of [" + sTag + "] : tag never started.");
 			return false;
 		}
 
 		// process.hrtime() returns the current high-resolution real time in a [seconds, nanoseconds] tuple Array
-		const hrDiff = process.hrtime(this._tests[sTag].timeStart);
+		const hrDiff = process.hrtime(self._tests[sTag].timeStart);
 		// we convert hrDiff in ms
 		// the first element of Array is time in secondes
 		// the second time tuple Array element is a number of nanoseconds and
 		// it has to be divided by 1,000,000 in order to get time in milliseconds.
 		const iDiff = Math.round(hrDiff[0] * 1000) + Math.round(hrDiff[1] / 1000000);
 
-		delete this._tests[sTag];
+		delete self._tests[sTag];
 
-		const indexSum = _.findIndex(this._testsSummary, {
+		const indexSum = _.findIndex(self._testsSummary, {
 			sName: sTag
 		});
 		chai.expect(indexSum, "indexSum devrait être > -1").to.be.a('number').and.above(-1);
 
-		this._testsSummary[indexSum].iDuration += iDiff;
+		self._testsSummary[indexSum].iDuration += iDiff;
 
 		// update min and max of duration
-		if ((iDiff <= this._testsSummary[indexSum].iMinDuration) || _.isNull(this._testsSummary[indexSum].iMinDuration)) {
-			this._testsSummary[indexSum].iMinDuration = iDiff;
+		if ((iDiff <= self._testsSummary[indexSum].iMinDuration) || _.isNull(self._testsSummary[indexSum].iMinDuration)) {
+			self._testsSummary[indexSum].iMinDuration = iDiff;
 		}
 
-		if (iDiff >= this._testsSummary[indexSum].iMaxDuration || _.isNull(this._testsSummary[indexSum].iMaxDuration)) {
-			this._testsSummary[indexSum].iMaxDuration = iDiff;
+		if (iDiff >= self._testsSummary[indexSum].iMaxDuration || _.isNull(self._testsSummary[indexSum].iMaxDuration)) {
+			self._testsSummary[indexSum].iMaxDuration = iDiff;
 		}
 
-		if (iDiff > this._testsSummary[indexSum].iLevel) {
-			this._testsSummary[indexSum].iNbAboveLevel++;
+		if (iDiff > self._testsSummary[indexSum].iLevel) {
+			self._testsSummary[indexSum].iNbAboveLevel++;
 		}
 
 		// mark the tag ended, in order to check the unended ones
-		this._testsSummary[indexSum].ended = true;
+		self._testsSummary[indexSum].ended = true;
 
 		let sDuration = "";
 
-		if (this._printStartAndEnd) {
+		if (self._printStartAndEnd) {
 
 			const sLogText = "\n%s executed in : %s";
 
-			sDuration = this.humanizeSeconds(iDiff);
+			sDuration = self.humanizeSeconds(iDiff);
 
 			return console.log(sLogText, sTag, sDuration);
 		}
@@ -232,6 +232,7 @@ const myPerfLogger = {
 				sOrderAttribute: 'iDuration',
 				sOrderSens: 'desc',
 				sTitle : "",
+				sTitle: "",
 				fCallback: null
 			};
 		}
@@ -271,8 +272,8 @@ const myPerfLogger = {
 		}
 
 		if (_.isUndefined(options.fCallback) || _.isNull(options.fCallback)) {
-			if (!_.isNull(this._fSummaryHookFunction) && !_.isUndefined(this._fSummaryHookFunction) && _.isFunction(this._fSummaryHookFunction)) {
-				options.fCallback = this._fSummaryHookFunction;
+			if (!_.isNull(self._fSummaryHookFunction) && !_.isUndefined(self._fSummaryHookFunction) && _.isFunction(self._fSummaryHookFunction)) {
+				options.fCallback = self._fSummaryHookFunction;
 			}
 		} else {
 			chai.expect(options.fCallback).to.be.a('function');
@@ -282,9 +283,9 @@ const myPerfLogger = {
 		chai.expect(options.sOrderSens, "sOrderSens devrait être une chaine définie").to.exist.and.be.a('string');
 		chai.expect(options.sTitle, "sTitle devrait être une chaine définie").to.exist.and.be.a('string');
 
-		if (this._isEnabled) {
+		if (self._isEnabled) {
 
-			if (this._testsSummary.length === 0) {
+			if (self._testsSummary.length === 0) {
 				console.log("perf.summary() : no test stored.");
 			}
 
@@ -358,13 +359,13 @@ const myPerfLogger = {
 			}];
 			let aRows = [];
 
-			let aSortedTests = _.orderBy(this._testsSummary, [options.sOrderAttribute], [options.sOrderSens]);
+			let aSortedTests = _.orderBy(self._testsSummary, [options.sOrderAttribute], [options.sOrderSens]);
 			let sErrors = "";
 
 			for (let i = 0; i < aSortedTests.length; i++) {
 
 				if (aSortedTests[i].ended === false) {
-					sErrors += "\nTag ["+aSortedTests[i].sName+"] never ended : perf measurement skipped for this tag.\n";
+					sErrors += "\nTag [" + aSortedTests[i].sName + "] never ended : perf measurement skipped for self tag.\n";
 					continue;
 				}
 
@@ -373,12 +374,12 @@ const myPerfLogger = {
 
 				const iDuration = aSortedTests[i].iDuration;
 
-				sDuration = this.humanizeSeconds(iDuration);
+				sDuration = self.humanizeSeconds(iDuration);
 
 				let iAvgDuration = 0;
 
 				iAvgDuration = Math.round(aSortedTests[i].iDuration / aSortedTests[i].iNb);
-				sAvgDuration = this.humanizeSeconds(iAvgDuration);
+				sAvgDuration = self.humanizeSeconds(iAvgDuration);
 
 				let sAboveLevel = "";
 				if (aSortedTests[i].iNbAboveLevel > 0) {
@@ -395,7 +396,7 @@ const myPerfLogger = {
 					sCriticity = "";
 				}
 
-				const aRow = [aSortedTests[i].sName, aSortedTests[i].iNb, sDuration, this.humanizeSeconds(aSortedTests[i].iMinDuration), this.humanizeSeconds(aSortedTests[i].iMaxDuration), sAvgDuration, this.humanizeSeconds(aSortedTests[i].iLevel), sCriticity, sAboveLevel];
+				const aRow = [aSortedTests[i].sName, aSortedTests[i].iNb, sDuration, self.humanizeSeconds(aSortedTests[i].iMinDuration), self.humanizeSeconds(aSortedTests[i].iMaxDuration), sAvgDuration, self.humanizeSeconds(aSortedTests[i].iLevel), sCriticity, sAboveLevel];
 				aRows.push(aRow);
 
 			} // END loop for ... on aSortedTest[]
@@ -406,7 +407,7 @@ const myPerfLogger = {
 				bHookDefined = true;
 			}
 
-			if (!bHookDefined || (bHookDefined && this._bDisplaySummaryWhenHook)) {
+			if (!bHookDefined || (bHookDefined && self._bDisplaySummaryWhenHook)) {
 				console.log("  -------------------------------------------------------------------" + _.repeat("-", options.sTitle.length));
 				console.log("                 Performance summary " + options.sTitle + moment().format('DD-MM-YYYY HH:mm'));
 				console.log("  -------------------------------------------------------------------" + _.repeat("-", options.sTitle.length));
@@ -425,15 +426,15 @@ const myPerfLogger = {
 			}
 
 			if (bHookDefined) {
-				objectSummary.aRows = _.filter(this._testsSummary, {ended:true});
+				objectSummary.aRows = _.filter(self._testsSummary, { ended: true });
 				options.fCallback(objectSummary);
 			}
 
 			if (options.bReset) {
 				aSortedTests = [];
 				aRows = [];
-				this._tests = {};
-				this._testsSummary = [];
+				self._tests = {};
+				self._testsSummary = [];
 			}
 
 			return;
@@ -456,7 +457,7 @@ const myPerfLogger = {
 
 		chai.expect(f).to.exist.and.be.a('function');
 
-		this._fSummaryHookFunction = f;
+		self._fSummaryHookFunction = f;
 
 	},
 
@@ -466,7 +467,7 @@ const myPerfLogger = {
 		chai.expect(iMs, "iMs should be a non empty number").to.exist.and.be.a('number').and.not.be.empty;
 
 		if (_.isEmpty(sTag)) {
-			sTag = this._LEVEL_TAG_ALL;
+			sTag = self._LEVEL_TAG_ALL;
 		}
 
 		const newLevel = {
@@ -474,10 +475,10 @@ const myPerfLogger = {
 			iDuration: iMs
 		};
 
-		this._levels.push(newLevel);
+		self._levels.push(newLevel);
 
 	}
 
 };
 
-module.exports = myPerfLogger;
+module.exports = self;
